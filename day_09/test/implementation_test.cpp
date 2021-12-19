06752cec0a7aca457980a4bb847728149ffb281a
Thunraz <thunraz@thunraz.com>
2021-12-19 16:14:53 +0100

    Add function body to splitAndConvertToNumbers

--- day_09/test/implementation_test.cpp
+++ day_09/test/implementation_test.cpp
@@ -1,23 +1,46 @@
 #include "../src/implementation.hpp"
-#include "gtest/gtest.h"
 #include "strutils.hpp"
+#include "gtest/gtest.h"
 #include <string>
 
 using namespace ::testing;
 
 std::vector<int> splitAndConvertToNumbers(std::string const& input)
 {
-    return std::vector<int>{};
+    if (input.empty()) {
+        return {};
+    }
+
+    std::vector<std::string> lines = strutil::split(input, "\n");
+
+    std::vector<int> output;
+    std::transform(lines.begin(), lines.end(), std::back_inserter(output),
+        [](auto const& str) { return std::stoi(str); });
+
+    return output;
 }
 
 int findFirstNumberWhichIsIllegal(std::string const& input, int preamble_size)
 {
-    std::vector<std::string> lines = strutil::split(input, "\n");
     std::vector<int> numbers = splitAndConvertToNumbers(input);
 
     return 127;
 }
 
+TEST(SplitAndConvertToNumbersTest, EmptyInputReturnsEmptyVector)
+{
+    std::string input = "";
+    EXPECT_TRUE(splitAndConvertToNumbers(input).empty());
+}
+
+TEST(SplitAndConvertToNumbersTest, InputWithTwoNumberReturnsCorrectValues)
+{
+    std::string input = "12\n34";
+    auto const numbers = splitAndConvertToNumbers(input);
+
+    EXPECT_EQ(numbers.at(0), 12);
+    EXPECT_EQ(numbers.at(1), 34);
+}
 
 TEST(ImplementationTest, ExampleTest)
 {
@@ -42,6 +65,5 @@ TEST(ImplementationTest, ExampleTest)
 309
 576)";
 
-	ASSERT_EQ(findFirstNumberWhichIsIllegal(input, 5), 127);
+    ASSERT_EQ(findFirstNumberWhichIsIllegal(input, 5), 127);
 }
-
