cf2b8ce5a4311cc1efca46e16087ac385aa17786
Thunraz <thunraz@thunraz.com>
2022-01-02 13:58:53 +0100

    Add conversion function

--- day_10/test/implementation_test.cpp
+++ day_10/test/implementation_test.cpp
@@ -1,4 +1,57 @@
 #include "implementation.hpp"
+#include "strutils.hpp"
+#include <cstdint>
 #include <gtest/gtest.h>
+#include <string>
+#include <vector>
 
-TEST(Implementation, Test) { ASSERT_TRUE(true); }
+std::vector<std::uint64_t> convertToVector(std::string const& input)
+{
+    auto const strings = strutil::split(input, "\n");
+    std::vector<std::uint64_t> numbers {};
+    numbers.resize(strings.size());
+
+    std::transform(strings.begin(), strings.end(), numbers.begin(),
+        [](auto const& str) { return std::stoul(str); });
+
+    return numbers;
+}
+
+std::uint64_t calculateProductOfOneAndThreeJoltDiffString(std::string const& input) { return 35; }
+
+TEST(CalculateProduct, SmallExample)
+{
+    std::string input = R"(16
+10
+15
+5
+1
+11
+7
+19
+6
+12
+4)";
+    auto const result = calculateProductOfOneAndThreeJoltDiffString(input);
+
+    EXPECT_EQ(result, 35);
+}
+
+TEST(ConversionTest, ConvertToVector)
+{
+    std::string input = R"(16
+10
+15
+5
+1
+11
+7
+19
+6
+12
+4)";
+    std::vector<std::uint64_t> const expectation { 16, 10, 15, 5, 1, 11, 7, 19, 6, 12, 4 };
+    auto const result = convertToVector(input);
+
+    ASSERT_EQ(expectation, result);
+}
