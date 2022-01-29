2d1a8a64013b1b303a1e0ea543474bb9c8653482
pk <paul@whirlingchair.com>
2022-01-29 09:26:09 -0500

    Initial test

--- day_11/test/implementation_test.cpp
+++ day_11/test/implementation_test.cpp
@@ -1,4 +1,32 @@
 #include "implementation.hpp"
 #include <gtest/gtest.h>
 
-TEST(CalculateProduct, SmallExample) { }
+struct Position {
+    int x, y;
+};
+
+
+
+class Ferry {
+ public:
+     Ferry(std::string const& input);
+
+     bool is_seat(Position const& p)
+     {
+         return false;
+     }
+};
+
+Ferry::Ferry(const std::string& input)
+{
+
+}
+
+
+TEST(ParseInput, SimpleInputIsParsedCorrectly) {
+    auto input = "L";
+    Ferry f(input);
+
+    ASSERT_TRUE(f.is_seat(Position{0, 0}));
+}
+
